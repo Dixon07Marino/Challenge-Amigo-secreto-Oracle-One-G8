e54363ade8f8ac1529fc6bfa694991a8a25c33c9
Dixon Marino <dmusicmsoccer14@gmail.com>
2025-01-21 21:34:37 -0500

    Juego terminado - El juego cumple con todos los requerimientos

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosLista = document.getElementById("listaAmigos")
+let amigoSecreto = document.getElementById("resultado")
+let titulo = document.querySelector("h2")
+let nombreAmigo = document.getElementById("amigo").focus()
+let amigos = []
+
+
+let index = 0
+let amigo;
+
+
+function escribirTitulo(texto) {
+    titulo.innerHTML = texto
+}
+
+function agregarAmigo(){
+    nombreAmigo = document.getElementById("amigo").value
+    if (nombreAmigo == "") {
+        escribirTitulo("¡Debes agregar un nombre valido!")
+    }
+    else {
+        escribirTitulo("¡Agrega más amigos si lo deseas!")
+        amigos.push(nombreAmigo)
+        while (index < amigos.length) {
+            amigo = amigos[index]
+            let elementoLi = document.createElement("li")
+            amigosLista.appendChild(elementoLi)
+            elementoLi.innerHTML = amigo
+            nombreAmigo = document.getElementById("amigo").value = ""
+            nombreAmigo = document.getElementById("amigo").focus()
+            index++
+        }
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        escribirTitulo("¡No has agregado amigos aún!")
+        nombreAmigo = document.getElementById("amigo").focus()
+    }
+    else {
+        let numeroRandom = Math.floor(Math.random() * amigos.length)
+        amigoSecreto.innerHTML = amigos[numeroRandom]
+    }
+}
